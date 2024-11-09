ce8a9be3a5820950b1274879895cf718934e7fa7
om salunke <omsaunke343@gmail.com>
2024-11-09 18:17:34 +0530

    typewriter_2 completed

--- src/app/typewriter/page.tsx
+++ src/app/typewriter/page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 import Typewriter_1 from "./_components/Typewriter_1";
+import TypewriterComponent from "./_components/Typewriter_2";
 
 const Page = () => {
   return (
-    <div className="h-[5vh]">
-      <Typewriter_1
+    <div className="">
+      {/* <Typewriter_1
         message="Follow the multi ui "
         cursorColor="green"
         duration={2}
         steps={10}
         className="text-4xl text-red-400 w-[auto]"
-      />
+      /> */}
+      <TypewriterComponent message="om salunke welcome" duration={2} className="w-[10vh]"/>
     </div>
   );
 };
