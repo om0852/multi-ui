b5b27a13797b94bddfd1efd956833befb1df5638
om salunke <omsaunke343@gmail.com>
2024-11-12 13:05:00 +0530

    InputField 5 added containing  two component for email and url

--- src/app/inputfields/page.tsx
+++ src/app/inputfields/page.tsx
@@ -1,26 +1,15 @@
-"use client"
-import React from 'react';
-import InputField_4 from './_components/InputField_4';
+"use client";
+import React from "react";
+import InputField_4 from "./_components/InputField_4";
+import UrlInput, { EmailInput } from "./_components/InputField_5";
 
 const Form = () => {
   return (
-    <form className='p-[10vh]'>
-      <InputField_4
-        name="name"
-        id="name"
-        placeholder="Enter your name"
-        label="Name"
-        error='om salunke'
-        required
-      />
-      {/* You can also add an image URL */}
-      {/* <InputField_4
-        name="email"
-        id="email"
-        placeholder="Enter your email"
-        label="Email"
-        required
-      /> */}
+    <form className="p-[10vh]">
+      <div className="w-[40vh]">
+        <UrlInput type="password" placeholder="red" />
+      </div>
+      <EmailInput />
     </form>
   );
 };
