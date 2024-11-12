a0a72c7aa84676da4cdb5552b8e3968205aa5e6d
om salunke <omsaunke343@gmail.com>
2024-11-12 13:35:31 +0530

    input field 6 added

--- src/app/inputfields/page.tsx
+++ src/app/inputfields/page.tsx
@@ -2,14 +2,16 @@
 import React from "react";
 import InputField_4 from "./_components/InputField_4";
 import UrlInput, { EmailInput } from "./_components/InputField_5";
+import InputField_6 from "./_components/InputField_6";
 
 const Form = () => {
   return (
     <form className="p-[10vh]">
-      <div className="w-[40vh]">
-        <UrlInput type="password" placeholder="red" />
-      </div>
-      <EmailInput />
+      <div className="w-[100%] h-[auto]">
+        {/* <UrlInput type="password" placeholder="red" /> */}
+        <InputField_6 label="Your Label" id="input-id" placeholder="Enter text"  />
+        </div>
+      {/* <EmailInput /> */}
     </form>
   );
 };
