49d44886dc5cfb1e4db8b6e5d07374146b5e7c0e
om salunke <omsaunke343@gmail.com>
2024-11-14 00:14:35 +0530

    inputfield 9 added

--- src/app/inputfields/page.tsx
+++ src/app/inputfields/page.tsx
@@ -5,15 +5,17 @@ import UrlInput, { EmailInput } from "./_components/InputField_5";
 import InputField_6 from "./_components/InputField_6";
 import NotifyForm from "./_components/InputField_7";
 import AnimatedInput from "./_components/InputField_8";
+import GradientInput from "./_components/InputField_9";
 
 const Form = () => {
   return (
     <div className="p-[10vh]">
-      <div className="w-[100%] h-[auto] px-[10vh]">
+      <div className="w-[100%] h-[40vh] px-[10vh]">
         {/* <UrlInput type="password" placeholder="red" /> */}
         {/* <InputField_6 label="Your Label" id="input-id" placeholder="Enter text"  /> */}
         {/* <NotifyForm width={"100vh"}/> */}
-        <AnimatedInput/>
+        {/* <AnimatedInput/> */}
+        <GradientInput placeholder="first name" height={"10vh"}  className="h-[20vh]" />
         </div>
       {/* <EmailInput /> */}
     </div>
