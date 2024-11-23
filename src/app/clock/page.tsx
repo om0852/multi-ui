661e03da7d5595ce482a1517f261722ecfbd6243
om salunke <omsaunke343@gmail.com>
2024-11-24 01:05:25 +0530

    Clock 5 and 6 added

--- src/app/clock/page.tsx
+++ src/app/clock/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import NumericClock from './_components/Clock_2'
+import NumericClock from './_components/Clock_7'
 import Clock3 from './_components/Clock_3'
 import DigitalClock from './_components/Clock_4'
 
 const Page = () => (
     <div>
-        {/* <NumericClock /> */}
+        <NumericClock />
         {/* <Clock3/>
          */}
-         <DigitalClock/>
+         {/* <DigitalClock/> */}
     </div>
 )
 
