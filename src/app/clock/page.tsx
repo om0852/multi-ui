756e4460dbb9249f295febc283cabb2b0d443d6a
om salunke <omsaunke343@gmail.com>
2024-11-24 00:43:18 +0530

    add clock 2 ,3 and 4

--- src/app/clock/page.tsx
+++ src/app/clock/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import NumericClock from './_components/Clock_2'
+import Clock3 from './_components/Clock_3'
+import DigitalClock from './_components/Clock_4'
 
-const Page = () => {
-  return (
+const Page = () => (
     <div>
-      
+        {/* <NumericClock /> */}
+        {/* <Clock3/>
+         */}
+         <DigitalClock/>
     </div>
-  )
-}
+)
 
 export default Page
