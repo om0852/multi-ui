e626e8bc4db6b2949773fc189357f7dcf5262a16
om salunke <omsaunke343@gmail.com>
2024-11-25 23:43:56 +0530

    Clock 8 and 9 added

--- src/app/clock/page.tsx
+++ src/app/clock/page.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import NumericClock from './_components/Clock_7'
 import Clock3 from './_components/Clock_3'
 import DigitalClock from './_components/Clock_4'
+import AnalogClock from './_components/Clock_8'
+import EnhancedAnalogClock from './_components/Clock_9'
 
 const Page = () => (
     <div>
-        <NumericClock />
+        {/* <NumericClock /> */}
         {/* <Clock3/>
          */}
          {/* <DigitalClock/> */}
-    </div>
+         {/* <AnalogClock size={300} borderColor="border-blue-500" /> */}
+         <EnhancedAnalogClock/>
+         </div>
 )
 
 export default Page
