7c6a74b0611b4d1665ca86f847db10d93fa46ac4
om salunke <omsaunke343@gmail.com>
2024-12-08 19:15:40 +0530

    two more design for stepped counter added

--- src/app/steppedprogressbar/page.tsx
+++ src/app/steppedprogressbar/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import SteppedProgressBar from "../steppedprogressbar/_components/SteppedProgressBar_3";
+import SteppedProgressBar from "../steppedprogressbar/_components/SteppedProgressBar_5";
 
 const page = () => {
     const [count,setCOunt]=useState(0);
@@ -12,12 +12,17 @@ const page = () => {
        <SteppedProgressBar
        
    activeStep={count}
-    //  steps={["Start", "Middle", "Almost Done", "Finish"]}
-     activeColor="bg-purple-600"
-     completedColor="bg-teal-400"
-     inactiveColor="bg-gray-300"
-     animation={{ opacity: 1, transition: { duration: 0.4 } }}
-  // animation={{ scale: 1.3, transition: { duration: 0.4 } }}
+   steps={[
+        { label: "Start", onClick: () => console.log("Clicked Step 1") },
+        { label: "Middle", onClick: () => console.log("Clicked Step 2") },
+        { label: "Almost Done", onClick: () => console.log("Clicked Step 3") },
+        { label: "Finish", onClick: () => console.log("Clicked Step 4") },
+      ]}
+      activeColor="bg-red-600"
+      completedColor="bg-yellow-400"
+      inactiveColor="bg-gray-300"
+      animation={{ rotate: 360, scale: 1.1, transition: { duration: 0.6 } }}
+    //  // animation={{ scale: 1.3, transition: { duration: 0.4 } }}
 />
 
     </div>
