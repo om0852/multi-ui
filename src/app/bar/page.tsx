7b22730637aa1eb2f4813e3aa9fdbf94acf02cb6
om salunke <omsaunke343@gmail.com>
2024-12-10 22:37:43 +0530

    Bar 1 to 7 added

---
+++ src/app/bar/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  {Component}  from './_components/Bar_7'
+
+const page = () => {
+  return (
+    <div>
+      <Component/>
+    </div>
+  )
+}
+
+export default page
