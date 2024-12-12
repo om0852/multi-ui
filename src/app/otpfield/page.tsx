ff076b3989f141f8b98e61aa85591d1c9b9ea435
om salunke <omsaunke343@gmail.com>
2024-12-13 00:29:06 +0530

    Otp fields done

---
+++ src/app/otpfield/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Example } from "./_components/OtpField_1";
+
+const page = () => {
+  return (
+    <div>
+      <Example />
+    </div>
+  );
+};
+
+export default page;
