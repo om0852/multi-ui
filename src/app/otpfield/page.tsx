2748833d1d1e6b68b4b9066f5cefdbc2d879b181
om salunke <omsaunke343@gmail.com>
2024-12-22 12:38:07 +0530

    all build error are solve

--- src/app/otpfield/page.tsx
+++ src/app/otpfield/page.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import { Example } from "./_components/OtpField_1";
+// import React from "react";
+// import { Example } from "./_components/OtpField_1";
+
+// const page = () => {
+//   return (
+//     <div>
+//       <Example />
+//     </div>
+//   );
+// };
+
+// export default page;
+import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <Example />
+      
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default page
