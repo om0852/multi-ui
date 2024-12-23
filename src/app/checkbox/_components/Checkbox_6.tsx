2691d5ee13dd9d5a34bd41a41d82f18b007da2b0
Hrushi4151 <hrushimore4151@gmail.com>
2024-12-23 13:32:31 +0530

    Card  ,checkbox, toggleswitch and loader added

---
+++ src/app/checkbox/_components/Checkbox_6.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+// Define the Card component as a functional React component with TypeScript
+type Checkboxprops = {}; // You can add props here if needed in the future
+
+const Checkbox: React.FC<Checkboxprops> = () => {
+  const [mounted, setMounted] = useState(false);
+
+  // Set mounted to true after the component is rendered on the client
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Don't render on the server during hydration
+  if (!mounted) return null;
+  return (
+    <StyledWrapper>
+      <label className="container">
+        <input defaultChecked={true} type="checkbox" />
+        <div className="checkmark" />
+      </label>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  .container input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  .container {
+    display: block;
+    position: relative;
+    cursor: pointer;
+    font-size: 30px;
+    user-select: none;
+  }
+
+  .checkmark {
+    position: relative;
+    top: 0;
+    left: 0;
+    height: 1.6em;
+    width: 1.6em;
+    border-radius: 50%;
+    background: #ffeded38;
+    transition: all 0.2s ease;
+  }
+
+  .checkmark {
+    opacity: 0.4;
+  }
+
+  .container input:checked ~ .checkmark {
+    background: linear-gradient(144deg,#af40ff,#5b42f3 50%,#00ddeb);
+    opacity: 0.9;
+    transition: all 0.2s ease;
+  }
+
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  .container input:checked ~ .checkmark:after {
+    display: block;
+  }
+
+  .container .checkmark:after {
+    left: 0.61em;
+    top: 0.43em;
+    width: 0.25em;
+    height: 0.5em;
+    border: solid rgb(255, 255, 255);
+    border-width: 0 0.15em 0.15em 0;
+    transform: rotate(45deg);
+  }`;
+
+export default Checkbox;
---
+++ src/app/checkbox/page.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Checkbox_1 from './_components/Checkbox_1';
+import Checkbox_2 from './_components/Checkbox_2';
+import Checkbox_3 from './_components/Checkbox_3';
+import Checkbox_4 from './_components/Checkbox_4';
+import Checkbox_5 from './_components/Checkbox_5';
+import Checkbox_6 from './_components/Checkbox_6';
+import Checkbox_7 from './_components/Checkbox_7';
+import Checkbox_8 from './_components/Checkbox_8';
+import Checkbox_9 from './_components/Checkbox_9';
+import Checkbox_10 from './_components/Checkbox_10';
+
+const App = () => {
+
+    const WalletIcon = () => (
+       <img className='z-10 h-fit w-fit ' src="https://www.svgrepo.com/show/805/customer-service.svg" />
+      );
+
+
+
+  return (
+    <div className='flex flex-col justify-around items-center space-y-28  gap-12 '>
+        
+       <Checkbox_2 />
+       <Checkbox_1 />
+       <Checkbox_3/>
+       <Checkbox_4/>
+       <Checkbox_5/>
+       <Checkbox_6/>
+       <Checkbox_7/>
+       <Checkbox_8/>
+       <Checkbox_9/>
+       <Checkbox_10/>
+      
+    </div>
+  );
+};
+
+export default App;
+
