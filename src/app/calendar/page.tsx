8c526aab7cb53ff1e73ed4f5909558c1371b03d6
om salunke <omsaunke343@gmail.com>
2024-12-24 11:57:47 +0530

    calendar 11 to 20 added

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import Calendar from './_components/Calendar_10'
+import Calendar from './_components/Calendar_20'
 
 const page = () => {
   return (
--- src/app/toggleswitch/page.tsx
+++ src/app/toggleswitch/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Toggle_1 from './_components/Toggle_1';
 import Toggle_2 from './_components/Toggle_2';
@@ -35,41 +34,3 @@ const App = () => {
 
 export default App;
 
-=======
-import React from 'react';
-import Toggle_1 from './_components/Toggle_1';
-import Toggle_2 from './_components/Toggle_2';
-import Toggle_3 from './_components/Toggle_3';
-import Toggle_4 from './_components/Toggle_4';
-import Toggle_5 from './_components/Toggle_5';
-import Toggle_6 from './_components/Toggle_6';
-import Toggle_7 from './_components/Toggle_7';
-import Toggle_8 from './_components/Toggle_8';
-import Toggle_9 from './_components/Toggle_9';
-import Toggle_10 from './_components/Toggle_10';
-
-const App = () => {
-
-   
-
-
-  return (
-    <div className='flex flex-col justify-around items-center space-y-28  gap-12 '>
-       <Toggle_1/>
-       <Toggle_2 />
-       <Toggle_3/>
-       <Toggle_4/>
-       <Toggle_5/>
-       <Toggle_6/>
-       <Toggle_7/>
-       <Toggle_8/>
-       <Toggle_9/>
-       <Toggle_10/>
-      
-    </div>
-  );
-};
-
-export default App;
-
->>>>>>> 7927750ba26b50dd1a0ece376cf45ab44c37e8dc
