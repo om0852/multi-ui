8815b733ae48dad37a8d168786a885ceb1910436
Hrushi4151 <hrushimore4151@gmail.com>
2024-12-24 19:18:38 +0530

    Tootltip Component added

---
+++ src/app/tooltip/page.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Tooltip_1 from './_components/Tooltip_1';
+import Tooltip_2 from './_components/Tooltip_2';
+import Tooltip_3 from './_components/Tooltip_3';
+import Tooltip_4 from './_components/Tooltip_4';
+import Tooltip_5 from './_components/Tooltip_5';
+import Tooltip_6 from './_components/Tooltip_6';
+import Tooltip_7 from './_components/Tooltip_7';
+import Tooltip_8 from './_components/Tooltip_8';
+import Tooltip_9 from './_components/Tooltip_9';
+import Tooltip_10 from './_components/Tooltip_10';
+
+const App = () => {
+
+   
+
+
+  return (
+    <div className='flex flex-col justify-around items-center space-y-28  gap-12 '>
+       <Tooltip_1/>
+       <Tooltip_2 />
+       <Tooltip_3/>
+       <Tooltip_4/>
+       <Tooltip_5/>
+       <Tooltip_6/>
+       <Tooltip_7/>
+       <Tooltip_8/>
+       <Tooltip_9/>
+       <Tooltip_10/>
+      
+    </div>
+  );
+};
+
+export default App;
+
