1553984451de33095eeace3ceffb1df7e2db641d
om salunke <omsaunke343@gmail.com>
2024-12-26 12:27:57 +0530

    Clock 13 to 15 added

--- src/app/clock/page.tsx
+++ src/app/clock/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 // import NumericClock from './_components/Clock_7'
 // import Clock3 from './_components/Clock_3'
-// import DigitalClock from './_components/Clock_4'
+import DigitalClock from './_components/Clock_13'
+import FlipClock from './_components/Clock_14'
+import AnalogClock from './_components/Clock_15'
 // import AnalogClock from './_components/Clock_8'
 // import EnhancedAnalogClock from './_components/Clock_10'
 // import EnhancedSquareClock from './_components/Clock_11'
@@ -12,8 +14,20 @@ const Page = () => (
         {/* <NumericClock /> */}
         {/* <Clock3/>
          */}
-         {/* <DigitalClock/> */}
-         {/* <AnalogClock size={300} borderColor="border-blue-500" /> */}
+
+{/* <FlipClock size="small" />
+<FlipClock size="medium" />
+
+      <FlipClock
+        size="large"
+        containerClassName="h-screen bg-black flex justify-center items-center"
+        digitClassName="text-yellow-400"
+        labelClassName="text-red-500"
+      />         */}
+      <AnalogClock size={300} />
+<AnalogClock size={100}  />
+
+       {/* <AnalogClock size={300} borderColor="border-blue-500" /> */}
          {/* <EnhancedAnalogClock/> */}
          {/* <EnhancedSquareClock/> */}
          </div>
