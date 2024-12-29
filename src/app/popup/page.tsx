13de3206b8511da9903eafe7fca5df38902d73b4
om salunke <omsaunke343@gmail.com>
2024-12-29 22:53:26 +0530

    Popup 21 to 24 added

--- src/app/popup/page.tsx
+++ src/app/popup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import PopupMenu from "./Components/Popup_20";
+import PopupMenu from "./Components/Popup_24";
 
 const App = () => {
   const menuItems = [
@@ -30,11 +30,10 @@ const App = () => {
       </h1>
       <PopupMenu
         menuItems={menuItems}
-        direction="top-right"
         distance={80}
         label="✨"
         centerColor="bg-green-500" // Pass a custom color for the center button
-        menuColor="bg-yellow-500" // Pass a custom color for the menu items
+        // menuColor="bg-yellow-500" // Pass a custom color for the menu items
       />
     </div>
   );
