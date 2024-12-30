0f56a48bbe06f58767152d8fcdbc67cb474d885e
om salunke <omsaunke343@gmail.com>
2024-12-30 09:56:42 +0530

    Progressbar 21 to 25 added

--- src/app/progressbar/page.tsx
+++ src/app/progressbar/page.tsx
@@ -1,21 +1,17 @@
 "use client"
 import React from "react";
-import ProgressBar from "./_components/ProgressBar_20";
+import TextBasedProgressBar from "./_components/ProgressBar_22";
 
 const Example1 = () => {
   return (
     <div className="p-4">
       <h2 className="text-xl font-semibold">Linear Progress Bar with Gradient Fill</h2>
-      <ProgressBar
-        progress={75} 
-        color="bg-gradient-to-r from-pink-500 to-yellow-500" 
-        backgroundColor="bg-gray-300" 
-        animationDuration={2} 
-        showCounter={true} 
-        // onStart={() => console.log("Animation started")} 
-        // onComplete={() => console.log("Animation completed")} 
-      />
-    </div>
+      <TextBasedProgressBar
+  progress={75} // Progress value (e.g., 75%)
+  text="Processing..." // Custom text to display
+  animationDuration={1} // Duration of animation in seconds
+  showCounter={true} // Show percentage counter
+/>    </div>
   );
 };
 
