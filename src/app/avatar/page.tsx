2e5a6248249c8a07d6395dc9312a2c68d4140449
om salunke <omsaunke343@gmail.com>
2025-01-01 13:52:46 +0530

    Avatar 1 to 5 added

---
+++ src/app/avatar/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Avatar from "./_components/Avatar_2";
+import RotatingRingAvatar from "./_components/Avatar_5";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="space-x-4">
+      <RotatingRingAvatar
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        alt="Avatar 1"
+        size="sm"
+      />
+      <RotatingRingAvatar
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        alt="Avatar 2"
+        size="md"
+      />
+      <RotatingRingAvatar
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        alt="Avatar 3"
+        size="lg"
+      />
+ </div>
+    </div>
+  );
+};
+
+export default App;
