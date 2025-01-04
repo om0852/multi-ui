7a4667c99af6469f0e1b9bd45260c829374b05b0
om salunke <omsaunke343@gmail.com>
2025-01-04 15:57:17 +0530

    Carsousel 2 to 5 added

--- src/app/carousel/page.tsx
+++ src/app/carousel/page.tsx
@@ -1,41 +1,25 @@
-"use client";
+import React from 'react'
+import  Carousel  from './_components/Carousel_5'
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselDots,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./_components/Carousel";
-
-export default function App() {
+const ExampleCarousel = () => {
   return (
-    <Carousel className="w-full h-64 bg-gray-100" interval={0} loop={true}>
-      {/* Carousel Slides */}
-      <CarouselContent transitionEffect={0}>
-        <CarouselItem className="bg-red-300">Slide 1</CarouselItem>
-        <CarouselItem className="bg-blue-300">Slide 2</CarouselItem>
-        <CarouselItem className="bg-green-300">Slide 3</CarouselItem>
-        <CarouselItem className="bg-purple-300">Slide 4</CarouselItem>
-        <CarouselItem className="bg-yellow-300">Slide 5</CarouselItem>
-        <CarouselItem className="bg-orange-300">Slide 6</CarouselItem>
-      </CarouselContent>
-
-      {/* Navigation Buttons */}
-      <CarouselPrevious className="text-white bg-black p-2 rounded">
-        {"<"}
-      </CarouselPrevious>
-      <CarouselNext className="text-white bg-black p-2 rounded">
-        {">"}
-      </CarouselNext>
-
-      {/* Carousel Dots */}
-      <CarouselDots
-        className="absolute bottom-4 w-full flex justify-center"
-        dotClassName="w-4 h-4 rounded-full bg-gray-300 mx-2 bg-red-300"
-        activeDotClassName="bg-blue-900"
-      />
-    </Carousel>
-  );
+    <div className="w-full max-w-lg mx-auto mt-10">
+      <Carousel autoPlay={true} interval={4000}>
+        <div className="w-full h-64 bg-red-500 flex items-center justify-center text-white font-bold text-xl">
+          Slide 1
+        </div>
+        <div className="w-full h-64 bg-blue-500 flex items-center justify-center text-white font-bold text-xl">
+          Slide 2
+        </div>
+        <div className="w-full h-64 bg-green-500 flex items-center justify-center text-white font-bold text-xl">
+          Slide 3
+        </div>
+        <div className="w-full h-64 bg-yellow-500 flex items-center justify-center text-black font-bold text-xl">
+          Slide 4
+        </div>
+      </Carousel>
+    </div>
+  )
 }
+
+export default ExampleCarousel
