d59eba0f1fce1944137ad913495002c41588a818
om salunke <omsaunke343@gmail.com>
2025-03-29 10:27:45 +0530

    test file updated again

--- app/test/Test_1.jsx
+++ app/test/Test_1.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-(() => { 
+const Test_1 = () => { 
     return (
       <div>
         <h1>Test_1</h1>
@@ -15,5 +14,7 @@ import React from 'react';
         <label>Test Label</label>
       </div>
     );
-  })
+  }
+
+  export default Test_1;
   
